cf5f2201b0744e7077864cc18937bf22ea1bca06
mt.khademi <mohande3khademi@gmail.com>
2025-02-20 09:31:12 +0330

    feat: add application db context

---
+++ src/Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Infrastructure.Context;
+
+public class ApplicationDbContext : IdentityDbContext<IdentityUser,
+    IdentityRole,
+    string, IdentityUserClaim<string>,
+    IdentityUserRole<string>,
+    IdentityUserLogin<string>,
+    IdentityRoleClaim<string>,
+    IdentityUserToken<string>>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+}
