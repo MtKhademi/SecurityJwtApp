7d526c4c662bad6036758e3125b28b91ef11b8cb
mt.khademi <mohande3khademi@gmail.com>
2025-02-20 10:34:56 +0330

    feat: add employee entity db config

---
+++ src/Infrastructure/DbConfigs/EmployeeConfig.cs
@@ -0,0 +1,14 @@
+﻿namespace Infrastructure.DbConfigs;
+
+internal class EmployeeConfig : IEntityTypeConfiguration<Employee>
+{
+    public void Configure(EntityTypeBuilder<Employee> builder)
+    {
+        builder.ToTable("Employees", SchemaNames.HR)
+            .HasIndex(e => e.FirstName)
+            .HasDatabaseName("IX_Employees_FirstName");
+
+        builder.HasIndex(e => e.LastName)
+            .HasDatabaseName("IX_Employees_LastName");
+    }
+}
