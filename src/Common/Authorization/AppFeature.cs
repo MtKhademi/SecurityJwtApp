e92f57e1a28e36d1fd66bc681e98e1c9a9a129ca
Vision3 <Mohande3khademi@gmail.com>
2025-02-20 12:25:06 +0330

    feat: add App feature constants

---
+++ src/Common/Authorization/AppFeature.cs
@@ -0,0 +1,10 @@
+namespace Common.Authorization;
+
+public static class AppFeature
+{
+    public const string Employees = nameof(Employees);
+    public const string Users = nameof(Users);
+    public const string Roles = nameof(Roles);
+    public const string UserRoles = nameof(UserRoles);
+    public const string RoleClaims = nameof(RoleClaims);
+}
--- src/Infrastructure/ServiceCollectionExtention.cs
+++ src/Infrastructure/ServiceCollectionExtention.cs
@@ -9,7 +9,8 @@ public static class ServiceCollectionExtention
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            // options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            options.UseInMemoryDatabase(Guid.NewGuid().ToString()));
 
         return services;
     }
