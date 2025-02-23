e266282f617f68d4425c8759734a47880fffda3b
Vision3 <Mohande3khademi@gmail.com>
2025-02-23 21:48:22 +0330

    feat: generate access token

---
+++ src/Application/AppConfigs/AppConfiguration.cs
@@ -0,0 +1,7 @@
+namespace Application.AppConfigs;
+
+public class AppConfiguration
+{
+    public string Secret { get; set; }
+    public TYPE TokenExpiryInMinutes { get; set; }
+}
--- src/Infrastructure/Services/Identity/TokenService.cs
+++ src/Infrastructure/Services/Identity/TokenService.cs
@@ -1,19 +1,130 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using Application.AppConfigs;
 using Application.Services.Identity;
 using Common.Requests;
 using Common.Responses;
 using Common.Responses.Wrappers;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Infrastructure.Services.Identity;
 
 public class TokenService : ITokenService 
 {
-    public Task<ResponseWrapper<TokenResponse>> GetTokenAsync(TokenRequest tokenRequest, CancellationToken cancellationToken = default)
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly RoleManager<ApplicationRole> _roleManager;
+    private readonly AppConfiguration _appConfiguration;
+
+    public TokenService(UserManager<ApplicationUser> userManager, 
+        RoleManager<ApplicationRole> roleManager,
+        IOptions<AppConfiguration> appConfiguration)
     {
-        throw new NotImplementedException();
+        _userManager = userManager;
+        _roleManager = roleManager;
+        _appConfiguration = appConfiguration.Value;
+    }
+
+    public async Task<ResponseWrapper<TokenResponse>> GetTokenAsync(TokenRequest tokenRequest, CancellationToken cancellationToken = default)
+    {
+        var user = await _userManager.FindByEmailAsync(tokenRequest.Email);
+        if(user is null)
+            return await ResponseWrapper<TokenResponse>.FailAsync("Invalid email address.");
+        if(!user.IsActive)
+            return await ResponseWrapper<TokenResponse>.FailAsync("User not active. Please contact the admin");
+        
+        if(!user.EmailConfirmed)
+            return await ResponseWrapper<TokenResponse>.FailAsync("Email not confirmed. Please contact the admin");
+        
+        var isPasswordValid = await _userManager.CheckPasswordAsync(user, tokenRequest.Password);
+        if(!isPasswordValid)
+            return await ResponseWrapper<TokenResponse>.FailAsync("Invalid password.");
+
+        user.RefreshToken = GenerateRefreshToken();
+        user.RefreshTokenExpiryDate = DateTime.Now.AddDays(7);
+
+        await _userManager.UpdateAsync(user);
+
+        return new ResponseWrapper<TokenResponse>()
+        {
+            IsSuccessful = true,
+            ResponseData = new TokenResponse()
+            {
+                Token =await GenerateJWTTokenAsync(user),
+                RefreshToken = user.RefreshToken,
+                RefreshTokenExpiryTime = user.RefreshTokenExpiryDate
+            }
+        };
     }
 
     public Task<ResponseWrapper<TokenResponse>> GetRefreshTokenAsync(RefreshTokenRequest refreshTokenRequest, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        if(refreshTokenRequest is null)
+            return await ResponseWrapper<>
+    }
+
+
+    private string GenerateRefreshToken()
+    {
+        var randomNumber = new byte[32];
+        using (var rnd = RandomNumberGenerator.Create())
+        {
+            rnd.GetBytes(randomNumber);
+        }
+        return Convert.ToBase64String(randomNumber);
+    }
+
+    private async Task<string> GenerateJWTTokenAsync(ApplicationUser user)
+    {
+       return GenerateEncryptedToken(GetSigningCredentials(),await GetClaimsAsync(user));
+    }
+
+    private string GenerateEncryptedToken(SigningCredentials signingCredentials, IEnumerable<Claim> claims)
+    {
+        var token = new JwtSecurityToken(claims: claims, 
+            expires: DateTime.UtcNow.AddMinutes(_appConfiguration.TokenExpiryInMinutes),
+            signingCredentials: signingCredentials);
+        
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var encryptedToken = tokenHandler.WriteToken(token);
+        return encryptedToken;
+    }
+
+    private SigningCredentials GetSigningCredentials()
+    {
+        var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_appConfiguration.Secret));
+        return new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+    }
+
+    private async Task<IEnumerable<Claim>> GetClaimsAsync(ApplicationUser user)
+    {
+        var userClaims = await _userManager.GetClaimsAsync(user);
+        var roles = await _userManager.GetRolesAsync(user);
+        var roleClaims = new List<Claim>();
+        var permissionClaims = new List<Claim>();
+
+        foreach (var role in roles)
+        {
+            roleClaims.Add(new Claim(ClaimTypes.Role, role));
+            var currentRole = await _roleManager.FindByNameAsync(role);
+            var allPermissionsForCurrentRole = await _roleManager.GetClaimsAsync(currentRole);
+            permissionClaims.AddRange(allPermissionsForCurrentRole);    
+        }
+
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.NameIdentifier, user.Id),
+            new(ClaimTypes.Email, user.Email),
+            new(ClaimTypes.Name, user.FirstName),
+            new(ClaimTypes.Surname, user.LastName),
+            new(ClaimTypes.MobilePhone, user.PhoneNumber ?? string.Empty),
+        };
+        
+        claims.AddRange(roleClaims);
+        claims.AddRange(permissionClaims);
+
+        return claims;
     }
 }
